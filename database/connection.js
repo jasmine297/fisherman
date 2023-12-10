feb9ac365776cefaab64db59716e50766eedf4a8
stevestrz <pro.stevestrz@gmail.com>
2023-12-10 15:34:38 +0100

    mongodb connection done

--- database/connection.js
+++ database/connection.js
@@ -1,27 +1,13 @@
 require('dotenv').config()
+const mongoose = require('mongoose')
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.DATABASE_CONNECTION;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Successfully connected to Mongo database!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+async function connect() {
+  mongoose.connect(process.env.DATABASE_CONNECTION, {})
+  mongoose.connection.once('open', () => {
+    console.log('MongoDB connection established');
+  }).on('error', (err) => {
+    console.log('MongoDB connection error : ', err);
+  })
 }
 
-module.exports = { run };
+module.exports = { connect };
