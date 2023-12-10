bee8178507568b6f523616a4dc69e99908e34b0b
stevestrz <pro.stevestrz@gmail.com>
2023-12-10 15:34:38 +0100

    mongodb connection done

--- database/connection.js
+++ database/connection.js
@@ -1,18 +1,10 @@
 require('dotenv').config()
-const mongoose = require('mongoose');
-const { Testmodel } = require("./models/testmodel")
+const mongoose = require('mongoose')
 
 async function connect() {
   mongoose.connect(process.env.DATABASE_CONNECTION, {})
   mongoose.connection.once('open', () => {
     console.log('MongoDB connection established');
-    // Create test model
-    const testmodel = new Testmodel({
-      name: "test-model-1"
-    })
-    testmodel.save().then(val => {
-      console.log("Test model created");
-    })
   }).on('error', (err) => {
     console.log('MongoDB connection error : ', err);
   })
