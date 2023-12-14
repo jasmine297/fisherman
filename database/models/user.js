9979f65891279f4db2456c1823b2d534686ccdca
stevestrz <pro.stevestrz@gmail.com>
2023-12-14 15:12:21 +0100

    AUTH DONE

--- database/models/user.js
+++ database/models/user.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    name: {
+    firstname: {
         type: String,
         required: true
     },
-    surname: {
+    lastname: {
         type: String,
         required: true
     },
@@ -66,6 +66,10 @@ const userSchema = new mongoose.Schema({
         required: true
     },
     password: {
+        type: String,
+        required: true
+    },
+    email: {
         type: String,
         required: true
     }
