f2b20f6dc186e7466ccbaa2a2c829c8a08cfdd46
kheira05 <81557099+kheira05@users.noreply.github.com>
2024-01-11 14:47:13 +0100

    certfication HTTPS local

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 
+const https = require('https')
+const fs = require('fs')
 const express = require('express')
 const dbConnection = require("./database/connection")
 const app = express()
@@ -7,6 +9,11 @@ const cors = require('cors')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const port = process.env.SERVER_PORT
+
+const keys = {
+    key: fs.readFileSync("./cert/localhost.key"),
+    cert: fs.readFileSync('./cert/localhost.crt')
+}
 
 app.use(cors())
 app.use(morgan('tiny'))
@@ -20,7 +27,9 @@ require("./endpoints/trip.routes")(app);
 require("./endpoints/reservation.routes")(app);
 require("./endpoints/fishinglogbook.routes")(app);
 
-app.listen(port, () => {
+const server = https.createServer(keys, app)
+
+server.listen(port, () => {
     console.log(`Server listening on port ${port}`)
     dbConnection.connect();
 })
